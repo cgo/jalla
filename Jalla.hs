78a517780e5dc0326fbaddb94f5b096a4a9a8166
Christian <github@goschs.de>
2012-03-19 23:19:41 +0100

    New Jalla top level module

---
+++ Jalla.hs
@@ -0,0 +1,13 @@
+module Jalla ( 
+  module Jalla.BLAS.Foreign.BlasOps
+  , module Jalla.BLAS.Foreign.LapackeOps
+  , module Jalla.Types
+  , module Jalla.Vector
+  , module Jalla.Matrix
+             ) where
+
+import Jalla.BLAS.Foreign.BlasOps
+import Jalla.BLAS.Foreign.LapackeOps
+import Jalla.Types
+import Jalla.Vector
+import Jalla.Matrix
