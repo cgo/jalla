bc86cbe1c83adeef7a0af2657d9ff158953cccf3
Christian <github@goschs.de>
2012-03-19 23:13:55 +0100

    Moved arbitrary instances to Jalla/Test.hs

--- tests/Test.hs
+++ tests/Test.hs
@@ -12,6 +12,7 @@ import Jalla.BLAS.Foreign.BlasOps
 import Jalla.Matrix
 import Jalla.Vector
 import Jalla.Types
+import Jalla.Test
 import System.Random
 
 
@@ -50,36 +51,6 @@ tests = [
 --instance (Random a, CMatrix mat a) => Random (mat a) where
 --  randomR (lo,hi) g = createMatrix $ matrixMap
 
-instance (Random e, RealFloat e) => Random (Complex e) where
-  random g = (a' :+ b', g')
-    where (a',g'') = random g
-          (b',g')  = random g''
-  randomR (a :+ b, c :+ d) g = (a' :+ b', g')
-    where (a', g'') = randomR (a,c) g
-          (b', g')  = randomR (b,d) g''
-
-
-class Bounds a where
-  minB :: a
-  maxB :: a
-
-
-instance (RealFloat a) => Bounds a where
-  minB = realToFrac (-1000) -- fromIntegral (minBound :: Int)
-  maxB = realToFrac (1000)  -- fromIntegral (maxBound :: Int)
-  
-instance (Bounds a, RealFloat a) => Bounds (Complex a) where
-  minB = (minB :+ minB)
-  maxB = (maxB :+ maxB)
-
-
-instance (Bounds e, BlasOps e, Random e) => Arbitrary (Matrix e) where
-  arbitrary = do
-    m   <- choose (1,100)
-    n   <- choose (1,100)
-    els <- vectorOf (m*n) (choose (minB, maxB))
-    return $ listMatrix (m,n) els
-    
 
 
 prop_frobNorm :: Matrix CDouble -> Bool
